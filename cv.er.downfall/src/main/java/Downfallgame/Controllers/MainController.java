099339c53c29d40013531a17cad27baa6c78fadd
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-02-05 18:28:27 +0000

    Controlller

---
+++ cv.er.downfall/src/main/java/Downfallgame/Controllers/MainController.java
@@ -0,0 +1,16 @@
+package Downfallgame.Controllers;
+
+import javax.faces.event.AbortProcessingException;
+import javax.faces.event.ActionEvent;
+import javax.faces.event.ActionListener;
+import javax.swing.JFrame;
+
+public class MainController extends JFrame implements ActionListener {
+
+    @Override
+    public void processAction(ActionEvent arg0) throws AbortProcessingException {
+        // TODO Auto-generated method stub
+
+    }
+    
+}
