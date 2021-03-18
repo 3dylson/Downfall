8ff59f7c7dde05c23897ef968e361a175651ed2d
Edylson Manuel Gonçalves Frederico <edylson_frederico@outlook.com>
2021-03-18 14:01:19 +0000

    fixed UML impl

--- cv.er.downfall/src/main/java/Downfallgame/BL/GameBoard.java
+++ cv.er.downfall/src/main/java/Downfallgame/BL/GameBoard.java
@@ -1,6 +1,5 @@
 package Downfallgame.BL;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,45 +7,46 @@ import java.util.List;
  * wheels.
  */
 public class GameBoard {
-    
+
     /** Stores game parameters (number of axles, game type, etc.) */
-    protected GameParameters GameParameters;
-    
+    protected GameParameters gameParameters;
     /** The current player. Game board is drawn based on this. */
-    protected Player CurrPlayer;
+    protected Player currPlayer;
 
     public List<Axle> m_Axle;
     public List<GameType> type;
     public List<Storage> m_Storage;
     public List<Collector> m_Collector;
-    
 
-    public GameBoard(){}
-    
-    /** Start a new game. Initialize game objects. */
-    public void NewGame() {
-        new Axle();
-        new Wheel();
-        new Storage();
-        new GameType();
-        new Collector();        
+    public GameBoard(GameParameters gameParameters, Player curPlayer, Axle mAxle, GameType type, Storage mStorage,
+            Collector mCollector) {
+        this.gameParameters = gameParameters;
+        this.currPlayer = curPlayer;
+
     }
 
-    /** Collector calls Game Over, if the game is finished. The winner player is indicated. */
+    /** Start a new game. Initialize game objects. */
+    public void NewGame() {
+
+    }
+
+    /**
+     * Collector calls Game Over, if the game is finished. The winner player is
+     * indicated.
+     */
     public void GameOver(Boolean WinOrLoose) {
 
     }
 
     /** Configure game parameters. */
-    public void ConfigureGame(){
-        
+    public void ConfigureGame() {
 
     }
 
     /** User input to turn the wheel to Angle. Returns False if not successful. */
-    public Boolean TurnWheel(int WheelNo, int Angle){
+    public Boolean TurnWheel(int WheelNo, int Angle) {
         return null;
 
     }
-    
+
 }
--- cv.er.downfall/src/main/java/Downfallgame/BL/Wheel.java
+++ cv.er.downfall/src/main/java/Downfallgame/BL/Wheel.java
@@ -1,52 +1,69 @@
 package Downfallgame.BL;
 
-import java.util.List;
-
 /**
  * A wheel has to turn a certain degree, during the turn it checks if there are
  * donors or acceptors.
  */
 public class Wheel extends Container {
 
+    /** the radius of the wheel. Necessary when drawing. */
     protected int Radius;
-    protected int Angle;
+    /** the angle of relative displacement at the moment. */
+    protected int Angle = 0;
 
-    public Axle Axle;
-
-    public Wheel(){}
-
-    public Wheel(int Radius, int Angle){
+    public Wheel(int Radius, int Angle) {
         this.Radius = Radius;
         this.Angle = Angle;
     }
 
-    public void TurnTo(Integer Angle) {
+    /**
+     * Turn wheel to angle. If slots are turned to neighbor angles, Receive and
+     * Offer messages are triggered.
+     * 
+     * @param Angle
+     */
+    public void TurnTo(int Angle) {
 
     }
 
-    protected Container TurnTill(Integer Angle) {
+    /**
+     * The wheel turns itself until either it reaches the specified angle, or it
+     * finds a neighbour being capable to receive a ball from or to give a ball to.
+     * 
+     * @param Angle
+     * @return
+     */
+    protected Container TurnTill(int Angle) {
         return null;
 
     }
 
+    /**
+     * Inserts the ball into the slot that is at the angle of the donor
+     * 
+     * @param donor
+     * @param ball
+     */
     protected void SetBall(Container donor, Ball ball) {
-        
-    }
-    
-    protected void DeleteBall(Ball ball) {
-        
+
     }
 
+    /**
+     * Deletes the given ball from the slot it has it
+     * 
+     * @param ball
+     */
+    protected void DeleteBall(Ball ball) {
+
+    }
 
     /**
      * 
      * @return the current angle.
      */
-    public Integer GetAngle() {
+    public int GetAngle() {
         return Angle;
-        
+
     }
-        
-    
-    
+
 }
